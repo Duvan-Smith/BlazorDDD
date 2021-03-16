451873c621f0455089b4ed142787a2c455e5df23
Duvan Smith Montenegro Roque <dsmith.mr@gmail.com>
2021-03-16 14:44:31 -0500

    Correcion Dto

--- Blazor.Aplicacion.Dto/OperacionesDto/DivisionDtos/DivisionRequestDto.cs
+++ Blazor.Aplicacion.Dto/OperacionesDto/DivisionDtos/DivisionRequestDto.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Blazor.Aplicacion.Dto.OperacionesDto.DivisionDtos
+﻿namespace Blazor.Aplicacion.Dto.OperacionesDto.DivisionDtos
 {
-    public class DivisionRequestDto: DivisionDto
+    public class DivisionRequestDto : DivisionDto
     {
-        public double Divisor { get; set; }
-        public double Dividendo { get; set; }
     }
 }
