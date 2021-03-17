f0127aa944912ff9b9553b087a58a4a5da490700
Duvan Smith Montenegro Roque <dsmith.mr@gmail.com>
2021-03-17 07:52:32 -0500

    end fachada controller

--- Blazor.WebApi/Controllers/FachadaUser/Fachada/FachadaUserController.cs
+++ Blazor.WebApi/Controllers/FachadaUser/Fachada/FachadaUserController.cs
@@ -1,4 +1,5 @@
 ﻿using Blazor.Aplicacion.Core.Users.FachadaUser;
+using Blazor.Aplicacion.Dto.UsersDto.InicioSesion;
 using Blazor.Aplicacion.Dto.UsersDto.Registro;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -18,9 +19,26 @@ namespace Blazor.WebApi.Controllers.FachadaUser.Fachada
             _logger = logger;
             _fachadaUserService = fachadaUserService;
         }
+        [HttpPost(nameof(LoginUser))]
+        public async Task<InicioSesionResponseDto> LoginUser(InicioSesionRequestDto requestDto) =>
+            await _fachadaUserService.UserLogin(requestDto).ConfigureAwait(false);
+
         [HttpPost(nameof(InsertUser))]
         public async Task<UserResponseDto> InsertUser(UserRequestDto userRequestDto) =>
             await _fachadaUserService.UserManagementInsert(userRequestDto).ConfigureAwait(false);
+
+        [HttpPost(nameof(UpdateUser))]
+        public async Task<UserResponseDto> UpdateUser(UserRequestDto userRequestDto) =>
+            await _fachadaUserService.UserManagementUpdate(userRequestDto).ConfigureAwait(false);
+
+        [HttpPost(nameof(DeleteUser))]
+        public async Task<UserResponseDto> DeleteUser(UserRequestDto userRequestDto) =>
+            await _fachadaUserService.UserManagementDelete(userRequestDto).ConfigureAwait(false);
+
+        [HttpPost(nameof(GetUser))]
+        public async Task<UserRequestDto> GetUser(UserRequestDto userRequestDto) =>
+            await _fachadaUserService.UserManagementGet(userRequestDto).ConfigureAwait(false);
+
         [HttpGet(nameof(GetAll))]
         public async Task<IEnumerable<UserRequestDto>> GetAll() =>
             await _fachadaUserService.UserManagementGetAll().ConfigureAwait(false);
